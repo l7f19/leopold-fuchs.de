b3841e8154559258f7f52c5eb4171524f275fcb9
l7f19 <fuchs.leopold@gmx.de>
2021-04-23 02:40:43 +0200

    restructured home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,36 +7,46 @@ export default function Home({ data }) {
     return (
         <Layout>
             <Seo title="Home" />
-            <div class="columns is-gapless">
-                <div className="column">
-                    <section class="hero is-fullheight is-link">
-                        <div class="hero-body">
-                            <div class="container has-text-centered">
-                                <p class="title is-1 pb-6">
-                                    Hi! I'm Leopold, a computer science student currently based in Stuttgart, Germany.
-                                </p>
-                                <p class="title is-3 mt-6">
-                                   At Baden-Wuerttemberg Cooperative State University Stuttgart I'm learning the required theoretical knowledge and I also gain practical experience working at IBM.
-                                </p>
-                            </div>
+                <section class="hero is-fullheight is-link">
+                    <div class="hero-body">
+                        <div class="container has-text-centered">
+                            <p class="title is-1 pb-6">
+                                Hi! I'm Leopold, a computer science student based in Stuttgart, Germany.
+                            </p>
+                            <p class="title is-2 mt-6">
+                                As a corporate student I'm ...
+                            </p>
+                            <p className="title is-3 mt-6">
+                                <div class="columns">
+                                    <div className="column">
+                                        studying at Baden-Wuerttemberg Cooperative State University Stuttgart.
+                                    </div>
+                                    <div className="column">
+                                        gaining practical experience working every semester within an internship at IBM.
+                                    </div>
+                                </div>
+                            </p>
                         </div>
-                    </section>
-                </div>
-                <div className="column">
-                    <section class="hero is-fullheight is-primary">
-                        <div class="hero-body">
-                            <div class="container has-text-centered">
-                                <p class="title is-3 mb-6">
-                                    I'm interested in fullstack web/app development. Frameworks that I'm working with are React, Gatsby and React Native for frontend and Express and MongoDB for backend development.
-                                </p>
-                                <p class="title is-1 pt-6">
-                                    On this website I want to show something <Link className="keyword" to="/cv">about me</Link> and <Link className="keyword" to="/projects">projects</Link> that I'm working on.
-                                </p>
-                            </div>
+                    </div>
+                </section>
+                <section class="hero is-fullheight is-primary">
+                    <div class="hero-body">
+                        <div class="container has-text-centered">
+                            <p class="title is-1 pb-6">
+                                On this website I want to show something <Link className="keyword" to="/cv">about me</Link> and <Link className="keyword" to="/projects">projects</Link> that I'm working on.
+                            </p>
+                            <p class="title is-2 pt-5 pb-4">
+                                I'm interested in fullstack web/app development and thereby mainly working with following technologies:
+                            </p>
+
+                            <p class="title is-2">
+                                React (Native) | Gatsby | ExpressJS | Google Firebase
+                            </p>
+                             
                         </div>
-                    </section>
-                </div>
-            </div>
+                    </div>
+                </section>
+
         </Layout>
     )
 }
