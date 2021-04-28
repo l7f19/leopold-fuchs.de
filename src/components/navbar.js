4d691bfd43a98979b07da5efc6157e3b955da2f3
l7f19 <fuchs.leopold@gmx.de>
2021-04-29 00:21:22 +0200

    added github and linkedin to navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,9 +7,23 @@ export default function Navbar() {
     
     const data = useStaticQuery(graphql`
         query {
-            file(relativePath: {eq: "logo.png"}) {
+            logo: file(relativePath: {eq: "logo.png"}) {
                 childImageSharp {
                     fixed(width: 50, height: 50) {
+                        ...GatsbyImageSharpFixed
+                    }
+                }
+            },
+            github: file(relativePath: {eq: "github.png"}) {
+                childImageSharp {
+                    fixed(width: 43 height: 43) {
+                        ...GatsbyImageSharpFixed
+                    }
+                }
+            },
+            linkedin: file(relativePath: {eq: "linkedin.png"}) {
+                childImageSharp {
+                    fixed(width: 41, height: 41) {
                         ...GatsbyImageSharpFixed
                     }
                 }
@@ -21,10 +35,7 @@ export default function Navbar() {
         <nav className="navbar is-fixed-top" role="navigation" aria-label="main navigation">
             <div className="navbar-brand m-0 p-0">
                 <Link className="ml-2 mt-1" to="/">      
-                    <Img
-                        fixed={data.file.childImageSharp.fixed}
-                        alt=""
-                    />
+                    <Img fixed={data.logo.childImageSharp.fixed} alt="" />
                 </Link>
                 <div
                     onClick={() => {
@@ -46,7 +57,7 @@ export default function Navbar() {
 
             <div id="navMenu" className={`navbar-menu ${isActive ? "is-active" : ""}`}>
                 <div className="navbar-end">
-                    <div className="navbar-item">
+                    <div className="navbar-item m4-6">
                         <Link to="/" className="navbar-item is-tab px-5">
                             Home
                         </Link>
@@ -60,6 +71,12 @@ export default function Navbar() {
                             Contact
                         </Link>
                     </div>
+                    <a href="https://github.com/l7f19" target="_blank" rel="noreferrer" className="pt-1 mt-1 ml-3">
+                        <Img fixed={data.github.childImageSharp.fixed} alt="" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/leopold-fuchs-3aa52a1bb/" target="_blank" rel="noreferrer" className="pt-1 mt-1 mx-3">
+                        <Img fixed={data.linkedin.childImageSharp.fixed} alt="" />
+                    </a>
                 </div>
             </div>
         </nav>
